9dbca37f0e5f6d35d423de964821fd36e299a471
Abhishek yadav <quantumabhishek713@gmail.com>
2026-01-11 18:34:16 +0530

    commit8

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ async def scrape_schemes(request: ScrapeRequest):
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
